939679a3007c3e9b9a8e4a04b1e8fd4289a32e69
cecep31 <cecepjanuardi31@gmail.com>
2021-09-11 22:58:26 +0700

    orm colection

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,9 @@
 @section('content')
 
     <div class="mb-4 border bottom-4 p-3 rounded-md shadow-sm bg-blue-100">
-        <h1 class="text-xl font-bold">{{$post["title"]}}</h1>
+        <div class="flex justify-center">
+            <h1 class="text-xl font-bold">{{$post["title"]}}</h1>
+        </div>
 
         <p class="mt-4">{{$post["body"]}}</p>
         <h5 class="mt-4">By: {{$post["author"]}}</h5>
